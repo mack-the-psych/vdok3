0a01353162f8c9e9f9cabe500d63481d30d9fa7e
Makoto Sano <Makoto.Sano@Mack-the-Psych.com>
2020-06-07 22:03:52 +0900

    Update Test_run

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,6 @@
 1. prep: Basic data preparation
 2. extract: Feature extraction
-3. process: Overlapping, distribution and other 
+3. process: Overlapping, distribution and more
 4. reorganize : Re-organizing for prediction
 5. train: Training models and validations
 6. test: Test models
--- process/overlapping.py
+++ process/overlapping.py
@@ -121,13 +121,26 @@ if __name__ == "__main__":
                          r'EN-Lemma-Passage-Def-PRE.csv', r'Question_ID', r'Question_ID_Sec')
     ovlpd.df_ac_overlapping_lemma.to_csv(r'../data/' + 'Overlapping-Lemma-Def-PRE.csv',
                                          encoding= 'latin1')
-    
     ovlpd.count_overlapping_synset(r'EN-Synset-Question-Def-PRE.csv')
     ovlpd.df_ac_overlapping_syn_lemma.to_csv(r'../data/' + 'Overlapping-Synset-Def-PRE.csv',
                                              encoding= 'latin1')    
+
+    # To save the memory space, the instance is re-created and count_overlapping() is to be re-run
+    ovlpd = odi_overlapping(r'EN-QA-Serialized-Def-ELVA.PILOT.PRE-TEST.csv',
+                            r'Questin_ID_Definition.csv', r'../data/', stop_words_d)
+    ovlpd.count_overlapping(r'EN-Lemma-Question-Def-PRE.csv', r'Student_Question_Index',
+                         r'Pre_Col_Name', r'Question_ID', r'Question_ID_Sec',
+                         r'EN-Lemma-Passage-Def-PRE.csv', r'Question_ID', r'Question_ID_Sec')
     ovlpd.count_overlapping_hypernyms(r'EN-Hypernyms-Question-Def-PRE.csv', stop_words_hy)
     ovlpd.df_ac_overlapping_hyper_lemma.to_csv(r'../data/' + 'Overlapping-Hypernyms-Def-PRE.csv',
                                                encoding= 'latin1')
+
+    # To save the memory space, the instance is re-created and count_overlapping() is to be re-run
+    ovlpd = odi_overlapping(r'EN-QA-Serialized-Def-ELVA.PILOT.PRE-TEST.csv',
+                            r'Questin_ID_Definition.csv', r'../data/', stop_words_d)
+    ovlpd.count_overlapping(r'EN-Lemma-Question-Def-PRE.csv', r'Student_Question_Index',
+                         r'Pre_Col_Name', r'Question_ID', r'Question_ID_Sec',
+                         r'EN-Lemma-Passage-Def-PRE.csv', r'Question_ID', r'Question_ID_Sec')
     ovlpd.count_overlapping_hyponyms(r'EN-Hyponyms-Question-Def-PRE.csv', stop_words_hy)
     ovlpd.df_ac_overlapping_hypo_lemma.to_csv(r'../data/' + 'Overlapping-Hyponyms-Def-PRE.csv',
                                               encoding= 'latin1')
